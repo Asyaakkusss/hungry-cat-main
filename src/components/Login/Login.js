164e27a800eba19366400d7a22402ce4493a3424
Asya <aya29@case.edu>
2024-04-06 19:43:15 -0400

    only approved users can log in and out

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,50 @@
 import React, { useState } from 'react';
 import './Login.css';
 import PropTypes from 'prop-types';
-import { loginUser } from '../api.js'
+import { loginUser } from '../api.js'; 
+import Button from '@mui/material/Button';
+
 
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
+  const approvedUsers = ["noah", "asya", "maisoon", "hart", "caio"]; 
+  const approvedPWDs = ["noahisawesome", 
+                        "asyaisawesome", 
+                        "maisoonisawesome", 
+                        "hartisawesome", 
+                        "caioisawesome"]
 
   const handleSubmit = async e => {
     e.preventDefault();
+
+    //returns -1 if the user is not found 
+    const userIndex = approvedUsers.indexOf(username);
+
+    if (userIndex !== -1 && approvedPWDs[userIndex] === password) {
     const token = await loginUser({
       username,
       password
     });
     setToken(token);
   }
+  else {
+    alert("you are not an approved user. fuck off!")
+  }
+  }
 
   return(
+    
     <div className="login-wrapper">
+      <div className="top-bar">
+      <div style={{ marginLeft: 'left' }} className="guest-option">
+        <Button style={{ marginLeft: 'left' }}>Continue as Guest</Button>
+      </div>
+      </div>
+      <div className="login-header">
       <h1>Please Log In</h1>
+      </div>
+     
     <form onSubmit={handleSubmit}>
       <label>
         <p>Username</p>
@@ -28,8 +54,8 @@ export default function Login({ setToken }) {
         <p>Password</p>
         <input type="password" onChange={e => setPassword(e.target.value)}/>
       </label>
-      <div>
-        <button type="submit">Submit</button>
+      <div className = "submit-button">
+        <Button type="submit">Submit</Button>
       </div>
     </form>
     </div>
